029a14117b037f602100772368401347e4d387b9
Nathan Hwang <thenoviceoof@gmail.com>
2012-07-06 22:48:10 -0400

    Add argument parsing, support interval and directory arguments

--- selfspy_webcam.py
+++ selfspy_webcam.py
@@ -3,22 +3,44 @@
 # selfspy_webcam.py
 # photologs through your webcam
 
+import argparse
+
 import os
-import cv
 import time
+import cv
 
-cv.StartWindowThread()
+if __name__=="__main__":
+    # get args
+    parser = argparse.ArgumentParser(description='Photolog through your webcam')
+    parser.add_argument('-i', '--interval', dest='interval', default=5,
+                        type=int, help=('Time interval in seconds between '
+                                        'snapshots'))
+    parser.add_argument('-s', '--size', dest='size', default='320x240',
+                        type=str, help=('Size of the stored image in <wxh> '
+                                        'format (ex. 640x480)'))
+    parser.add_argument('-p', '--password', dest='password', default=None,
+                        help='')
+    parser.add_argument('--dir', dest='dir', default=None,
+                        help='')
+    parser.add_argument('-d', '--daemonize', dest='daemonize',
+                        const=True, default=False, action='store_const',
+                        help='Enable automatic daemonization')
+    args = parser.parse_args()
 
-cap = cv.CaptureFromCAM(0)
+    # start photologging
+    cap = cv.CaptureFromCAM(0)
 
-home = os.getenv("HOME")
-directory = "%s/.selfspy/photolog/" % home
-if not os.path.exists(directory):
-    os.makedirs(directory)
+    home = os.getenv("HOME")
+    if args.dir is None:
+        directory = "%s/.selfspy/photolog/" % home
+    else:
+        directory = args.dir
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-for i in range(2):
-    f = cv.QueryFrame(cap)
-    timestamp = time.strftime("%FT%T.jpg").replace("-","_").replace(":","_")
-    path = directory + timestamp
-    cv.SaveImage(path, f)
-    time.sleep(2)
+    while True:
+        f = cv.QueryFrame(cap)
+        timestamp = time.strftime("%FT%T.jpg").replace("-","_").replace(":","_")
+        path = directory + timestamp
+        cv.SaveImage(path, f)
+        time.sleep(args.interval)
